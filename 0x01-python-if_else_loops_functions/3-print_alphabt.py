79f2ae0801f06371e9e7b30010784f397fdfe1d7
mershark <113654948+mershark@users.noreply.github.com>
2023-01-04 20:59:52 -0300

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for sletter in range(97, 123):
-    if chr(sletter) is not 'q' and chr(sletter) is not 'e':
-        print("{}".format(chr(sletter)), end="")
+for char in range(97, 123):
+    if char == 101 or char == 113:
+        continue
+    print('{:c}'.format(char), end="")

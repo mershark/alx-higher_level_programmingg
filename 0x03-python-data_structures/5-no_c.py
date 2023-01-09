c5f0e1324f19d50aa90aa52eeda431e55fce137a
mershark <113654948+mershark@users.noreply.github.com>
2023-01-09 16:25:11 -0300

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python3
-"""
-removes all characters c and C from a string
-"""
-
-
+#!/usr/bin/python3
 def no_c(my_string):
-
-    new = [x for x in my_string if x != 'c' and x != 'C']
-    return "".join(new)
+    output = my_string.translate({ord(i): None for i in 'cC'})
+    return output

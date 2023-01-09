54f903362d84171a3a71724a31dccaa1aa7da9dc
mershark <113654948+mershark@users.noreply.github.com>
2023-01-09 16:23:32 -0300

    Update 0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-
-
 def print_list_integer(my_list=[]):
-
-        for x in my_list:
-
-                print("{:d}".format(x))
+    for i in my_list:
+        print("{:d}".format(i))

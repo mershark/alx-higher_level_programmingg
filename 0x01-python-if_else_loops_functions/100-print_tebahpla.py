65658a31ebff8c934e3219348e806b24323c0d2b
mershark <113654948+mershark@users.noreply.github.com>
2023-03-08 19:06:20 -0300

    corrected

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for char in range(97, 123, -1):
-	if char % 2 == 0:
-		print('{:c}'.format(char - 32), end='')
-	print('{:c}'.format(char), end='')
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0

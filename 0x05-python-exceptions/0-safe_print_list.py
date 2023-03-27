6c17e5b40a8c8e9a6af7cdf75ad1117937e50a01
mershark <mersharktakyi@gmail.com>
2023-03-27 12:21:24 -0700

    first taks completed

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
 
     Args:
         my_list (list): The list to print elements from.
-        x (int): The number of elements of my_list to print.
+        x (int): The number of elements of my_list to be  print.
 
     Returns:
         The number of elements printed.
